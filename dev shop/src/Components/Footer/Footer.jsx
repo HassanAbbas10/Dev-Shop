952257eb509820a3c5f40e3c25076adf74479098
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2023-08-08 13:28:45 +0500

    C
    progress in project

--- dev shop/src/Components/Footer/Footer.jsx
+++ dev shop/src/Components/Footer/Footer.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import SendIcon from '@mui/icons-material/Send';
-import './Footer.scss'
+import React from "react";
+import SendIcon from "@mui/icons-material/Send";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='top'>
-        <input className='search' type='search' placeholder='Email' />
-        <button className='button-em'><SendIcon/></button>
-      </div>
-     
-      <div className='bottom'>
-      <img className='footer-img' src="Img/payment.png" alt="" />
-      </div>
-    </div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <h4>Subscribe to our emails</h4>
 
-export default Footer
+        {/* {I have take this input style idea from the Dean Hidri in code pen } */}
+        <div className="container">
+          <div className="container__item">
+            <form className="form">
+              <input
+                type="email"
+                className="form__field"
+                placeholder="Your E-Mail Address"
+              />
+              <button
+                type="button"
+                className="btn btn--primary btn--inside uppercase"
+              >
+                <SendIcon />
+              </button>
+            </form>
+          </div>
+        </div>
+        {/* {---------------------------------------------------------------------------} */}
+      </div>
+
+      <div className="bottom">
+        <img className="footer-img" src="Img/payment.png" alt="" />
+      </div>
+      <p>© 2023, Dev Shop Powered by Stripe</p>
+    </div>
+  );
+};
+
+export default Footer;
