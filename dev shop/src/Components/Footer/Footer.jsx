aa7a26e6ce2d890e5195a4a4d2b40e4b33b19837
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2023-08-08 12:35:23 +0500

    next

--- dev shop/src/Components/Footer/Footer.jsx
+++ dev shop/src/Components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import SendIcon from '@mui/icons-material/Send';
+import './Footer.scss'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='left'>
+      <div className='top'>
         <input className='search' type='search' placeholder='Email' />
         <button className='button-em'><SendIcon/></button>
       </div>
+     
       <div className='bottom'>
-      <img src="Img/payment.png" alt="" />
+      <img className='footer-img' src="Img/payment.png" alt="" />
       </div>
     </div>
   )
